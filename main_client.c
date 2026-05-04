89496296cc69ffcb6d1acc3334da7425d4c4888f
david03-ops <david.josepha03@gmail.com>
2026-05-04 18:05:58 -0400

    updated includes

---
+++ main_client.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <time.h>
+#include <signal.h> // Added for SIGPIPE handling
